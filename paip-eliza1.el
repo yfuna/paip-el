2bb1a444aa53c7fb4aeb355723d4b4f277653df5
Yosuke Funahashi <yosuke@funahashi.cc>
2014-05-10 02:01:55 +0900

    paip-eliza1.el works

--- paip-eliza1.el
+++ paip-eliza1.el
@@ -1,5 +1,10 @@
 ;; paip-eliza1.el
 
+(eval-when-compile
+  (require 'cl-lib))
+
+(require 'paip)
+
 ;;; -*- Mode: Lisp; Syntax: Common-Lisp; -*-
 ;;; Code from Paradigms of Artificial Intelligence Programming
 ;;; Copyright (c) 1991 Peter Norvig
@@ -10,141 +15,257 @@
 
 ;; New version of pat-match with segment variables
 
-(defun variable-p (x)
-  "Is x a variable (a symbol beginning with `?')?"
-  (and (symbolp x) (equal (elt (symbol-name x) 0) #\?)))
+;; (defun variable-p (x)
+;;   "Is x a variable (a symbol beginning with `?')?"
+;;   (and (symbolp x) (equal (elt (symbol-name x) 0) #\?)))
 
-(defun pat-match (pattern input &optional (bindings no-bindings))
+(defun paip-eliza1-variable-p (x)
+  "Is x a variable (a symbol beginning with `!')?"
+  (and (symbolp x) (equal (elt (symbol-name x) 0) ?!)))
+
+;; (defun pat-match (pattern input &optional (bindings no-bindings))
+;;   "Match pattern against input in the context of the bindings"
+;;   (cond ((eq bindings fail) fail)
+;;         ((variable-p pattern)
+;;          (match-variable pattern input bindings))
+;;         ((eql pattern input) bindings)
+;;         ((segment-pattern-p pattern)                ; ***
+;;          (segment-match pattern input bindings))    ; ***
+;;         ((and (consp pattern) (consp input)) 
+;;          (pat-match (rest pattern) (rest input)
+;;                     (pat-match (first pattern) (first input) 
+;;                                bindings)))
+;;         (t fail)))
+
+(cl-defun paip-eliza1-pat-match (pattern input &optional (bindings paip-no-bindings))
   "Match pattern against input in the context of the bindings"
-  (cond ((eq bindings fail) fail)
-        ((variable-p pattern)
-         (match-variable pattern input bindings))
+  (cond ((eq bindings paip-fail) paip-fail)
+        ((paip-eliza1-variable-p pattern)
+         (paip-match-variable pattern input bindings))
         ((eql pattern input) bindings)
-        ((segment-pattern-p pattern)                ; ***
-         (segment-match pattern input bindings))    ; ***
+        ((paip-eliza1-segment-pattern-p pattern)                ; ***
+         (paip-eliza1-segment-match pattern input bindings))    ; ***
         ((and (consp pattern) (consp input)) 
-         (pat-match (rest pattern) (rest input)
-                    (pat-match (first pattern) (first input) 
+         (paip-eliza1-pat-match (rest pattern) (rest input)
+                    (paip-eliza1-pat-match (first pattern) (first input) 
                                bindings)))
-        (t fail)))
+        (t paip-fail)))
 
-(defun segment-pattern-p (pattern)
+;; (defun segment-pattern-p (pattern)
+;;   "Is this a segment matching pattern: ((?* var) . pat)"
+;;   (and (consp pattern)
+;;        (starts-with (first pattern) '?*)))
+
+(defun paip-eliza1-segment-pattern-p (pattern)
   "Is this a segment matching pattern: ((?* var) . pat)"
   (and (consp pattern)
-       (starts-with (first pattern) '?*)))
+       (paip-starts-with (first pattern) '?*)))
 
 ;;; ==============================
 
-(defun segment-match (pattern input bindings &optional (start 0))
+;; (defun segment-match (pattern input bindings &optional (start 0))
+;;   "Match the segment pattern ((?* var) . pat) against input."
+;;   (let ((var (second (first pattern)))
+;;         (pat (rest pattern)))
+;;     (if (null pat)
+;;         (match-variable var input bindings)
+;;         ;; We assume that pat starts with a constant
+;;         ;; In other words, a pattern can't have 2 consecutive vars
+;;         (let ((pos (position (first pat) input
+;;                              :start start :test #'equal)))
+;;           (if (null pos)
+;;               fail
+;;               (let ((b2 (pat-match pat (subseq input pos) bindings)))
+;;                 ;; If this match failed, try another longer one
+;;                 ;; If it worked, check that the variables match
+;;                 (if (eq b2 fail)
+;;                     (segment-match pattern input bindings (+ pos 1))
+;;                     (match-variable var (subseq input 0 pos) b2))))))))
+
+(cl-defun paip-eliza1-segment-match (pattern input bindings &optional (start 0))
   "Match the segment pattern ((?* var) . pat) against input."
   (let ((var (second (first pattern)))
         (pat (rest pattern)))
     (if (null pat)
-        (match-variable var input bindings)
+        (paip-match-variable var input bindings)
         ;; We assume that pat starts with a constant
         ;; In other words, a pattern can't have 2 consecutive vars
         (let ((pos (position (first pat) input
-                             :start start :test #'equal)))
+                             :start start :test 'equal)))
           (if (null pos)
-              fail
-              (let ((b2 (pat-match pat (subseq input pos) bindings)))
+              paip-fail
+              (let ((b2 (paip-eliza1-pat-match pat (subseq input pos) bindings)))
                 ;; If this match failed, try another longer one
                 ;; If it worked, check that the variables match
-                (if (eq b2 fail)
-                    (segment-match pattern input bindings (+ pos 1))
-                    (match-variable var (subseq input 0 pos) b2))))))))
+                (if (eq b2 paip-fail)
+                    (paip-eliza1-segment-match pattern input bindings (+ pos 1))
+                    (paip-match-variable var (subseq input 0 pos) b2))))))))
 
 ;;; ==============================
 
-(defun segment-match (pattern input bindings &optional (start 0))
-  "Match the segment pattern ((?* var) . pat) against input."
+;; (defun segment-match (pattern input bindings &optional (start 0))
+;;   "Match the segment pattern ((?* var) . pat) against input."
+;;   (let ((var (second (first pattern)))
+;;         (pat (rest pattern)))
+;;     (if (null pat)
+;;         (match-variable var input bindings)
+;;         ;; We assume that pat starts with a constant
+;;         ;; In other words, a pattern can't have 2 consecutive vars
+;;         (let ((pos (position (first pat) input
+;;                              :start start :test #'equal)))
+;;           (if (null pos)
+;;               fail
+;;               (let ((b2 (pat-match
+;;                           pat (subseq input pos)
+;;                           (match-variable var (subseq input 0 pos)
+;;                                           bindings))))
+;;                 ;; If this match failed, try another longer one
+;;                 (if (eq b2 fail)
+;;                     (segment-match pattern input bindings (+ pos 1))
+;;                     b2)))))))
+
+(cl-defun paip-eliza1-segment-match (pattern input bindings &optional (start 0))
+  "Match the segment pattern ((!* var) . pat) against input."
   (let ((var (second (first pattern)))
         (pat (rest pattern)))
     (if (null pat)
-        (match-variable var input bindings)
+        (paip-match-variable var input bindings)
         ;; We assume that pat starts with a constant
         ;; In other words, a pattern can't have 2 consecutive vars
-        (let ((pos (position (first pat) input
-                             :start start :test #'equal)))
+        (let ((pos (cl-position (first pat) input
+                             :start start :test 'equal)))
           (if (null pos)
-              fail
-              (let ((b2 (pat-match
+              paip-fail
+	    (let ((b2 (paip-eliza1-pat-match
                           pat (subseq input pos)
-                          (match-variable var (subseq input 0 pos)
-                                          bindings))))
+                          (paip-match-variable
+			   var (subseq input 0 pos)
+			   bindings))))
                 ;; If this match failed, try another longer one
-                (if (eq b2 fail)
-                    (segment-match pattern input bindings (+ pos 1))
+                (if (eq b2 paip-fail)
+                    (paip-eliza1-segment-match pattern input bindings (+ pos 1))
                     b2)))))))
 
 ;;; ==============================
 
-(defun rule-pattern (rule) (first rule))
-(defun rule-responses (rule) (rest rule))
+;; (defun rule-pattern (rule) (first rule))
+
+(defun paip-eliza1-rule-pattern (rule) (first rule))
+
+;; (defun rule-responses (rule) (rest rule))
+
+(defun paip-eliza1-rule-responses (rule) (rest rule))
 
 ;;; ==============================
 
-(defparameter *eliza-rules*
- '((((?* ?x) hello (?* ?y))      
-    (How do you do.  Please state your problem.))
-   (((?* ?x) I want (?* ?y))     
-    (What would it mean if you got ?y)
-    (Why do you want ?y) (Suppose you got ?y soon))
-   (((?* ?x) if (?* ?y)) 
-    (Do you really think its likely that ?y) (Do you wish that ?y)
-    (What do you think about ?y) (Really-- if ?y))
-   (((?* ?x) no (?* ?y))
-    (Why not?) (You are being a bit negative)
-    (Are you saying "NO" just to be negative?))
-   (((?* ?x) I was (?* ?y))       
-    (Were you really?) (Perhaps I already knew you were ?y)
-    (Why do you tell me you were ?y now?))
-   (((?* ?x) I feel (?* ?y))     
-    (Do you often feel ?y ?))
-   (((?* ?x) I felt (?* ?y))     
-    (What other feelings do you have?))))
+;; (defparameter *eliza-rules*
+;;  '((((?* ?x) hello (?* ?y))      
+;;     (How do you do.  Please state your problem.))
+;;    (((?* ?x) I want (?* ?y))     
+;;     (What would it mean if you got ?y)
+;;     (Why do you want ?y) (Suppose you got ?y soon))
+;;    (((?* ?x) if (?* ?y)) 
+;;     (Do you really think its likely that ?y) (Do you wish that ?y)
+;;     (What do you think about ?y) (Really-- if ?y))
+;;    (((?* ?x) no (?* ?y))
+;;     (Why not?) (You are being a bit negative)
+;;     (Are you saying "NO" just to be negative?))
+;;    (((?* ?x) I was (?* ?y))       
+;;     (Were you really?) (Perhaps I already knew you were ?y)
+;;     (Why do you tell me you were ?y now?))
+;;    (((?* ?x) I feel (?* ?y))     
+;;     (Do you often feel ?y ?))
+;;    (((?* ?x) I felt (?* ?y))     
+;;     (What other feelings do you have?))))
+
+(defvar paip-eliza1-*eliza-rules*
+  '((((!* !x) hello (!* !y))      
+     (How do you do.  Please state your problem.))
+    (((!* !x) I want (!* !y))     
+     (What would it mean if you got !y)
+     (Why do you want !y) (Suppose you got !y soon))
+    (((!* !x) if (!* !y)) 
+     (Do you really think its likely that !y) (Do you wish that !y)
+     (What do you think about !y) (Really-- if !y))
+    (((!* !x) no (!* !y))
+     (Why not!) (You are being a bit negative)
+     (Are you saying "NO" just to be negative!))
+    (((!* !x) I was (!* !y))       
+     (Were you really!) (Perhaps I already knew you were !y)
+     (Why do you tell me you were !y now!))
+    (((!* !x) I feel (!* !y))     
+     (Do you often feel !y !))
+    (((!* !x) I felt (!* !y))     
+     (What other feelings do you have!))))
 
 ;;; ==============================
 
-(defun eliza ()
+;; (defun eliza ()
+;;   "Respond to user input using pattern matching rules."
+;;   (loop
+;;     (print 'eliza>)
+;;     (write (flatten (use-eliza-rules (read))) :pretty t)))
+
+(defun paip-gps-eliza ()
   "Respond to user input using pattern matching rules."
   (loop
     (print 'eliza>)
-    (write (flatten (use-eliza-rules (read))) :pretty t)))
+    (write (paip-flatten
+	    (paip-use-eliza-rules (read))) :pretty t)))
 
-(defun use-eliza-rules (input)
+;; (defun use-eliza-rules (input)
+;;   "Find some rule with which to transform the input."
+;;   (some #'(lambda (rule)
+;;             (let ((result (pat-match (rule-pattern rule) input)))
+;;               (if (not (eq result fail))
+;;                   (sublis (switch-viewpoint result)
+;;                           (random-elt (rule-responses rule))))))
+;;         *eliza-rules*))
+
+(defun paip-eliza-use-eliza-rules (input)
   "Find some rule with which to transform the input."
-  (some #'(lambda (rule)
-            (let ((result (pat-match (rule-pattern rule) input)))
-              (if (not (eq result fail))
-                  (sublis (switch-viewpoint result)
-                          (random-elt (rule-responses rule))))))
-        *eliza-rules*))
+  (cl-some (lambda (rule)
+	     (let ((result (paip-eliza1-pat-match
+			    (paip-eliza1-rule-pattern rule) input)))
+	       (if (not (eq result paip-fail))
+		   (sublis (paip-eliza1-switch-viewpoint result)
+			   (paip-random-elt
+			    (paip-eliza1-rule-responses rule))))))
+	   paip-eliza-*eliza-rules*))
 
-(defun switch-viewpoint (words)
+;; (defun switch-viewpoint (words)
+;;   "Change I to you and vice versa, and so on."
+;;   (sublis '((I . you) (you . I) (me . you) (am . are))
+;;           words))
+
+(defun paip-eliza1-switch-viewpoint (words)
   "Change I to you and vice versa, and so on."
-  (sublis '((I . you) (you . I) (me . you) (am . are))
-          words))
+  (cl-sublis '((I . you) (you . I) (me . you) (am . are))
+	     words))
 
 ;;; ==============================
 
-(defun flatten (the-list)
-  "Append together elements (or lists) in the list."
-  (mappend #'mklist the-list))
+;; (defun flatten (the-list)
+;;   "Append together elements (or lists) in the list."
+;;   (mappend #'mklist the-list))
+;; [YF] Defined in the paip module.
 
-(defun mklist (x)
-  "Return x if it is a list, otherwise (x)."
-  (if (listp x)
-      x
-      (list x)))
+;; (defun mklist (x)
+;;   "Return x if it is a list, otherwise (x)."
+;;   (if (listp x)
+;;       x
+;;       (list x)))
+;; [YF] Defined in the paip module.
 
-(defun mappend (fn the-list)	
-  "Apply fn to each element of list and append the results."
-  (apply #'append (mapcar fn the-list)))
+;; (defun mappend (fn the-list)	
+;;   "Apply fn to each element of list and append the results."
+;;   (apply #'append (mapcar fn the-list)))
+;; [YF] Defined in the paip module.
 
-(defun random-elt (choices)
-  "Choose an element from a list at random."
-  (elt choices (random (length choices))))
+;; (defun random-elt (choices)
+;;   "Choose an element from a list at random."
+;;   (elt choices (random (length choices))))
+;; [YF] Defined in the paip module.
 
 ;;; ==============================
 
@@ -169,3 +290,4 @@
 
 ;;; ==============================
 
+(provide 'paip-eliza1)
--- paip-examples.el
+++ paip-examples.el
@@ -446,32 +446,32 @@
   "The program was named after the heroine of Pygmalion, who was taught to"
   "speak proper English by a dedicated teacher."
   (:section "5.2 Pattern Matching")
-  ((requires "eliza1"))
+  ((require 'paip-eliza1))
   "The hard part is the notion of pattern matching and transformation."
   "All symbols beginning with ? are variables for the pattern matcher."
   "First we see how to substitute variable/value pairs into expressions:"
-  ((sublis '((?X . vacation)) '(what would it mean to you if you got a ?X ? ))
+  ((cl-sublis '((!X . vacation)) '(what would it mean to you if you got a !X ? ))
    => (what would it mean to you if you got a VACATION ? ) @ 156)
   "Now a version of pat-match that works with such pairs:"
-  ((pat-match '(I need a ?x) '(I need a vacation))  @ 158)
+  ((paip-eliza1-pat-match '(I need a !x) '(I need a vacation))  @ 158)
   "Showing how to plug it in:"
-  ((sublis (pat-match '(I need a ?x) '(I need a vacation)) 
-	   '(what would it mean to you if you got a ?X ? ))
+  ((cl-sublis (paip-eliza1-pat-match '(I need a !x) '(I need a vacation)) 
+	      '(what would it mean to you if you got a !X ? ))
    => (what would it mean to you if you got a VACATION ? ) @ 159)
-  ((pat-match '(I need a ?x) '(I really need a vacation)) => nil)
-  ((pat-match '(this is easy) '(this is easy)) => ((t . t)))
-  ((pat-match '(?x is ?x) '((2 + 2) is 4)) => nil)
-  ((pat-match '(?x is ?x) '((2 + 2) is (2 + 2))) => ((?x 2 + 2)))
-  ((pat-match '(?P need . ?X) '(I need a long vacation))
-   => ((?X a long vacation) (?P . I)))
+  ((paip-eliza1-pat-match '(I need a !x) '(I really need a vacation)) => nil)
+  ((paip-eliza1-pat-match '(this is easy) '(this is easy)) => ((t . t)))
+  ((paip-eliza1-pat-match '(!x is !x) '((2 + 2) is 4)) => nil)
+  ((paip-eliza1-pat-match '(!x is !x) '((2 + 2) is (2 + 2))) => ((!x 2 + 2)))
+  ((paip-eliza1-pat-match '(!P need . !X) '(I need a long vacation))
+   => ((!X a long vacation) (!P . I)))
 
   (:section "5.3 Segment Pattern Matching")
   "We show how to have a variable that will match more than one element."
-  "We call these segment variables, and denote them (?* name)."
-  ((pat-match '((?* ?p) need (?* ?x))
-	      '(Mr Hulot and I need a vacation)) @ 160)
+  "We call these segment variables, and denote them (!* name)."
+  ((paip-eliza1-pat-match '((!* !p) need (!* !x))
+			'(Mr Hulot and I need a vacation)) @ 160)
   (:section "5.4 The Eliza Program: A Rule-Based Translator")
-  ((requires "eliza"))
+;;  ((require 'paip-eliza))
   "We can't show you an interactive ELIZA session, because the replies are"
   "random, and thus change every time.  You can experiment on your own by"
   "evaluating (ELIZA) and typing in your end of the conversation.
